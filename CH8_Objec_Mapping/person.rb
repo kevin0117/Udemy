828b1655ae7c0136230c34e359055c0c3310d12d
Kevin Wang <kevin0117@gmail.com>
2020-03-23 01:16:35 +0800

    CH8_#148_Reading_Objects_from_File

--- CH8_Objec_Mapping/person.rb
+++ CH8_Objec_Mapping/person.rb
@@ -2,14 +2,49 @@ require 'csv'
 
 class Person
   attr_accessor :first_name, :last_name, :person_id
-  def initialize (first_name, last_name)
+  def initialize (first_name, last_name, person_id = rand(1000..9999))
     @first_name = first_name
     @last_name = last_name
-    @person_id = rand(1000..9999)
+    @person_id = person_id
   end
   def valid?
     (!first_name.nil? && first_name.length > 0) && (!last_name.nil? && last_name.length > 0)
   end
+
+  def self.read(person_id)
+    filename = "#{person_id}-file.csv"
+    if File.exist?(filename)
+      File.open(filename,'r') do |f|
+        employee_name = CSV.parse(f.read).flatten
+        first_name = employee_name[0]
+        last_name = employee_name[1]
+
+        return Person.new(first_name, last_name, person_id)
+      end
+    else
+      puts "The #{filename} doesn't exist" 
+    end
+    # My version of show
+    # CSV.open("#{person_id}-file.csv",'r') do |csv|
+    #   csv.readline
+    # end
+  end
+
+
+#   def show
+#     filename = "#{person_id}-file.csv"
+#     if File.exist?(filename)
+#       File.open(filename,'r') do |f|
+#         puts f.read
+#       end
+#     else
+#       puts "The #{filename} doesn't exist" 
+#     end
+#     # My version of show
+#     # CSV.open("#{person_id}-file.csv",'r') do |csv|
+#     #   csv.readline
+#     # end
+#   end
 
   def save
     filename = "#{person_id}-file.csv"
@@ -21,7 +56,3 @@ class Person
     return !save_file.nil?
   end
 end
-
-person1 = Person.new('Kevin','Wang')
-
-p person1.save
