961dcd8c96961ce4c4a00724bd09939e646ea98f
Kevin Wang <kevin0117@gmail.com>
2020-03-22 20:51:15 +0800

    CH8_#144_Converting_Object_to_CSV

--- CH8_Objec_Mapping/person.rb
+++ CH8_Objec_Mapping/person.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Person
   attr_accessor :first_name, :last_name
 #   def initialize (first_name, last_name)
@@ -9,12 +11,10 @@ class Person
     (!first_name.nil? && first_name.length > 0) && (!last_name.nil? && last_name.length > 0)
   end
 
-  def walk
-      
-  end
-
-  def talk
-      
+  def to_csv
+    CSV.generate do |csv|
+      csv << [first_name, last_name]
+    end
   end
 end
 
@@ -22,4 +22,4 @@ person1 = Person.new
 person1.first_name = 'Bob'
 person1.last_name = 'Lee'
 
-puts person1.valid?
+p person1.to_csv
