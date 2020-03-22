dcf7c453fae4afa8bb4216e4dea013875ff515e6
Kevin Wang <kevin0117@gmail.com>
2020-03-22 21:37:43 +0800

    CH8_#146_Writing_Object_to_File

--- CH8_Objec_Mapping/person.rb
+++ CH8_Objec_Mapping/person.rb
@@ -1,25 +1,24 @@
 require 'csv'
 
 class Person
-  attr_accessor :first_name, :last_name
-#   def initialize (first_name, last_name)
-#     @first_name = first_name
-#     @last_name = last_name
-#   end
+  attr_accessor :first_name, :last_name, :person_id
 
   def valid?
     (!first_name.nil? && first_name.length > 0) && (!last_name.nil? && last_name.length > 0)
   end
 
-  def to_csv
-    CSV.generate do |csv|
+  def save
+    # If we got back the successful object from here, that means the file is save successfully 
+    save_file = CSV.open("#{person_id}-file.csv", "w") do |csv|
       csv << [first_name, last_name]
     end
+    return !save_file.nil?
   end
 end
 
 person1 = Person.new
 person1.first_name = 'Bob'
 person1.last_name = 'Lee'
+person1.person_id = '1001'
 
-p person1.to_csv
+p person1.save
