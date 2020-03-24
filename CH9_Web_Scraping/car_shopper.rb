a9e2ac590951fb72e982cda87cbf80aa2496a99f
Kevin Wang <kevin0117@gmail.com>
2020-03-24 17:05:36 +0800

    CH9_#168_JSON_Formatting

--- CH9_Web_Scraping/car_shopper.rb
+++ CH9_Web_Scraping/car_shopper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'httparty'
 require 'nokogiri'
 require 'pp'
@@ -9,37 +11,38 @@ def car_shopper
 
   car_listings = parsed_html.css('.card.car')
 
-#-------------------------------My Version-----------------------------------------
-#   car_listings.each do |each_car|
-#     car_name = each_car.css('.make').text
-#     car_year = each_car.css('.year').text
-#     car_rating = each_car.css('.rating').attribute("data-rating").value
-#     car_price = each_car.css('.price').text
-#
-#     car_info_list.push({name: car_name, year: car_year, rating: car_rating, price: car_price})
-#   end
-#
-#------------------------------Instructor Version-----------------------
+  #-------------------------------My Version-----------------------------------
+  #   car_listings.each do |each_car|
+  #     car_name = each_car.css('.make').text
+  #     car_year = each_car.css('.year').text
+  #     car_rating = each_car.css('.rating').attribute("data-rating").value
+  #     car_price = each_car.css('.price').text
+  #
+  #     car_info_list.push({name: car_name, year: car_year, rating: car_rating, price: car_price})
+  #   end
+  #
+  #------------------------------Instructor Version-----------------------
   cars = car_listings.map do |each_car|
-    { 
+    {
       name: each_car.css('.make').text,
-      year: each_car.css('year').text,
-      price: each_car.css('price').text,
-      rating: each_car.css('.star.rating').attribute("data-rating").value
+      year: each_car.css('.year').text.to_i,
+      price: each_car.css('.price').text,
+      rating: each_car.css('.star.rating').attribute('data-rating').value.to_i
     }
   end
 
-  File.open("car_listings.json", "wb") do |f|
-    f << cars.to_json # '<' is append, '<<' is overwrite 
+  File.open('car_listings.json', 'wb') do |f|
+    f << cars.to_json # '<' is append, '<<' is overwrite
   end
-# pp cars
-#   pp car_listings.first
-#   puts "-------------------------------------------------"
-#   puts car_listings.first
-#   puts "Welcome to the car shopper"
+  puts 'Results were written to file!'
+  # pp cars
+  #   pp car_listings.first
+  #   puts "-------------------------------------------------"
+  #   puts car_listings.first
+  #   puts "Welcome to the car shopper"
 
-#   puts response.body
-#   puts parsed_html
+  #   puts response.body
+  #   puts parsed_html
 end
 
 car_shopper
