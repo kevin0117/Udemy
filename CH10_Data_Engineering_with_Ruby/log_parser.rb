22aa12e6ef98dbbed471e58629bde916871917e2
Kevin Wang <kevin0117@gmail.com>
2020-03-25 13:24:07 +0800

    CH10_#177_Extracting_Use_Data

--- CH10_Data_Engineering_with_Ruby/log_parser.rb
+++ CH10_Data_Engineering_with_Ruby/log_parser.rb
@@ -9,8 +9,12 @@ signup_log_lines = access_log_lines.select do |line|
                      line.include?('/signup?email=')
                    end
 
-# puts signup_log_lines
+user_data = signup_log_lines.map do |line|
+              parsed_line = line.split('" "').last
+            end 
 
+puts user_data
 File.open('access_log.json','w') do |f|
-  f.write signup_log_lines
+  f.write user_data
+#   f.write signup_log_lines
 end
