633c9bbd8d72285ca2cb378fd8d51e345dd98067
Kevin Wang <kevin0117@gmail.com>
2020-03-25 13:00:29 +0800

    CH10_#176_Parsing_Log_File

--- CH10_Data_Engineering_with_Ruby/log_parser.rb
+++ CH10_Data_Engineering_with_Ruby/log_parser.rb
@@ -1,10 +1,16 @@
+require 'pp'
 filename = 'data/access_log_20190521-211058.log'
 
-access_log = File.open(filename, 'r') do |f|
-  f.readline
+access_log_lines = File.open(filename, 'r') do |f|
+  f.readlines
 end
 
-# puts access_log.length
+signup_log_lines = access_log_lines.select do |line|
+                     line.include?('/signup?email=')
+                   end
 
-puts access_log
+# puts signup_log_lines
 
+File.open('access_log.json','w') do |f|
+  f.write signup_log_lines
+end
