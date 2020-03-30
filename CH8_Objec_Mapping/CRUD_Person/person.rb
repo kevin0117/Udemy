a2974cee712ba049e40f7450da000ae5ac7fe961
Kevin Wang <kevin0117@gmail.com>
2020-03-30 10:47:08 +0800

    Class example

---
+++ CH8_Objec_Mapping/CRUD_Person/person.rb
@@ -0,0 +1,103 @@
+require 'csv'
+
+class Person
+  attr_accessor :first_name, :last_name, :person_id
+  attr_reader :filename
+
+  def initialize (first_name, last_name, person_id = rand(1000..9999))
+    @first_name = first_name
+    @last_name  = last_name
+    @person_id  = person_id
+    @filename   = "#{person_id}-file.csv"
+  end
+
+  def self.filename(person_id)
+    "#{person_id}-file.csv"
+  end
+
+  def valid?
+    if (!first_name.nil? && first_name.length > 0) && (!last_name.nil? && last_name.length > 0)
+      return true
+    else
+      print "Person attributes not valid"
+      return false
+    end
+  end
+
+  def save
+    unless valid?
+      puts "Cannot save file."
+    end
+    # If we got back the successful object from here, that means the file is save successfully 
+    save_file = CSV.open(filename, "w") do |csv|
+      csv << [first_name, last_name]
+    end
+    puts "File #{filename} saved for the employee with ID #{person_id}"
+    return !save_file.nil?
+  end
+
+  def self.read(person_id)
+    # filename = "#{person_id}-file.csv"
+    filename = Person.filename(person_id)
+    if File.exist?(filename)
+      File.open(filename,'r') do |f|
+        employee_name = CSV.parse(f.read).flatten
+        first_name = employee_name[0]
+        last_name = employee_name[1]
+
+        return Person.new(first_name, last_name, person_id)
+      end
+    else
+      puts "The #{filename} doesn't exist" 
+    end
+  end
+
+  def create    
+    if !File.exist?(filename) && valid?
+      save
+    else
+      puts "Cannot create file. File #{filename} already exists for #{person_id}"
+      return false
+    end
+  end
+
+  def update
+    # filename   = "#{person_id}-file.csv"
+    if File.exist?(filename)
+       save
+    else
+      puts "The #{filename} file doesn't exist, update cannot be performed" 
+      return false
+    end
+  end
+  
+  def self.destroy(person_id)
+    filename = Person.filename(person_id)
+    if File.exist?(filename)
+      File.delete(filename)
+      puts "The file was destroyed"
+      return true
+    else
+      "The #{filename} file doesn't exist, destroy cannot be performed" 
+      return false
+    end
+  end
+end
+
+
+
+
+#   def show
+#     filename = "#{person_id}-file.csv"
+#     if File.exist?(filename)
+#       File.open(filename,'r') do |f|
+#         puts f.read
+#       end
+#     else
+#       puts "The #{filename} doesn't exist" 
+#     end
+#     # My version of show
+#     # CSV.open("#{person_id}-file.csv",'r') do |csv|
+#     #   csv.readline
+#     # end
+#   end
---
+++ CH8_Objec_Mapping/CRUD_Person/runner.rb
@@ -0,0 +1,33 @@
+require_relative 'person'
+
+# person2 = Person.new('Jean','Chiu')
+
+# person2.save
+
+
+
+# p Person.update(2490, "Kevin", "Wang")
+# person1 = Person.new("Jean", "Chiu", 2490)
+# person1.save
+# person1.first_name = "Kevin"
+# person1.last_name = "Wang"
+
+# person1 = Person.read(2490)
+# # person1.first_name = "Kevin"
+# person1.last_name = "Chiu"
+# person1.update
+
+# person = Person.new("List", "Jones", '2265')
+
+# p person.create
+
+person = Person.read(2265)
+
+# p Person.destroy(2265)
+
+person.last_name = "Annie"
+
+p person.update
+
+
+
